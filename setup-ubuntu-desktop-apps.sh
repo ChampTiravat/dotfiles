39243900d8af900369be5ce5b50fb70e7263267a
champtiravat <tiravat2016@gmail.com>
2019-02-13 16:47:34 +0700

    :sparkles: Seperated desktop apps installation to other files

---
+++ setup-ubuntu-desktop-apps.sh
@@ -0,0 +1,8 @@
+echo "==================================="
+echo "Installing common Desktop apps"
+echo "==================================="
+sudo apt-get install -y gnome-disk-utility
+sudo apt-get install -y blender
+sudo apt-get install -y gimp
+sudo apt-get install -y kdenlive
+sudo apt-get install -y inkscape
