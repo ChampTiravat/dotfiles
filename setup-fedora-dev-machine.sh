1b855c8e539653f1a620755a2a6dc495940e193f
champtiravat <tiravat2016@gmail.com>
2019-02-13 16:48:21 +0700

    :hammer: Changed os package mananger for Fedora

--- setup-fedora-dev-machine.sh
+++ setup-fedora-dev-machine.sh
@@ -1,37 +1,37 @@
 echo "==================================="
 echo "Remove nano text editor"
 echo "==================================="
-sudo apt-get remove -y nano
+sudo dnf remove -y nano
 
 echo "==================================="
 echo "Replace vim.tiny with vim"
 echo "==================================="
-sudo apt-get remove -y vim.tiny
-sudo apt-get install -y vim
+sudo dnf remove -y vim.tiny
+sudo dnf install -y vim
 
 echo "==================================="
 echo "Installing untility softwares"
 echo "==================================="
-sudo apt-get install -y tmux
-sudo apt-get install -y zsh
-sudo apt-get install -y htop
-sudo apt-get install -y ranger
-sudo apt-get install -y git
-sudo apt-get install -y curl
+sudo dnf install -y tmux
+sudo dnf install -y zsh
+sudo dnf install -y htop
+sudo dnf install -y ranger
+sudo dnf install -y git
+sudo dnf install -y curl
 
 echo "==================================="
 echo "Installing misc softwares"
 echo "==================================="
-sudo apt-get install -y screenfetch
-sudo apt-get install -y cowsay
-sudo apt-get install -y fortune
+sudo dnf install -y screenfetch
+sudo dnf install -y cowsay
+sudo dnf install -y fortune
 
 echo "==================================="
 echo "Installing Python development softwares"
 echo "==================================="
-sudo apt-get install -y python3-pip
+sudo dnf install -y python3-pip
 
 echo "==================================="
 echo "Installing Golang development softwares"
 echo "==================================="
-sudo apt-get install -y go-dep
+sudo dnf install -y go-dep
