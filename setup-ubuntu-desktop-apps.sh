59d1833b163b9c6d2ee69fdd6d13ae3d28e8f80f
ChampTiravat <tiravat2016@gmail.com>
2023-04-13 06:30:41 +0700

    updated ubuntu setup script

--- setup-ubuntu-desktop-apps.sh
+++ setup-ubuntu-desktop-apps.sh
@@ -2,7 +2,7 @@ echo "==================================="
 echo "Installing common Desktop apps"
 echo "==================================="
 sudo apt-get install -y gnome-disk-utility
+sudo apt-get install -y gnome-tweak
 sudo apt-get install -y blender
 sudo apt-get install -y gimp
 sudo apt-get install -y kdenlive
-sudo apt-get install -y inkscape
