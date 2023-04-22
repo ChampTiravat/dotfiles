f798fcd5dbbafffe97ba8406501bbe8352b7d6bd
ChampTiravat <tiravat2016@gmail.com>
2023-04-23 04:06:15 +0700

    added krita to the list

--- setup-ubuntu-desktop-apps.sh
+++ setup-ubuntu-desktop-apps.sh
@@ -4,5 +4,6 @@ echo "==================================="
 sudo apt-get install -y gnome-disk-utility
 sudo apt-get install -y gnome-tweak
 sudo apt-get install -y blender
+sudo apt-get install -y krita
 sudo apt-get install -y gimp
 sudo apt-get install -y kdenlive
