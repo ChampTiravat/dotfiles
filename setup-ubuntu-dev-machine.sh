287a87b3f4c8f0e5bdb1ac6fa07757ccafac90e8
ChampTiravat <tiravat2016@gmail.com>
2024-09-21 11:13:39 +0700

    remove dups

--- setup-ubuntu-dev-machine.sh
+++ setup-ubuntu-dev-machine.sh
@@ -47,14 +47,36 @@ sudo apt-get install -y python3-pip
 echo "==================================="
 echo "Install VSCodium (opensourced version of VSCode"
 echo "==================================="
-
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
-
 sudo apt-get update -y
-
 sudo apt-get install -y codium
+
+echo "==================================="
+echo "Installing common Desktop apps"
+echo "==================================="
+sudo apt-get install -y gnome-disk-utility
+sudo apt-get install -y gnome-tweak
+sudo apt-get install -y blender
+sudo apt-get install -y krita
+sudo apt-get install -y gimp
+sudo apt-get install -y kdenlive
+
+echo "==================================="
+echo "Installing youtube downloader"
+echo "===================================";
+sudo add-apt-repository ppa:tomtomtom/yt-dlp
+sudo apt-get update -y
+sudo apt-get install -y yt-dlp
+
+echo "==================================="
+echo "Installing brave browser"
+echo "==================================="
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt-get update -y
+sudo apt-get install -y brave-browser
+
