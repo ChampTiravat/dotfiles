643f90414ab41d524d0b70d081aaaf44abd2b178
ChampTiravat <tiravat2016@gmail.com>
2024-09-21 15:17:16 +0700

    updated setup script

--- setup-ubuntu-dev-machine.sh
+++ setup-ubuntu-dev-machine.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
 
-echo "==================================="
-echo "Update system apps and apt repository"
-echo "==================================="
-sudo apt-get update -y
+echo "====================================================="
+echo "become root user for simplicity and conventien"
+echo "====================================================="
+sudo su
+
+echo "====================================================="
+echo "update system apps and apt repository"
+echo "====================================================="
+sudo apt-get update  -y
 sudo apt-get upgrade -y
 
-echo "==================================="
-echo "Remove nano text editor"
-echo "==================================="
+echo "====================================================="
+echo "remove nano text editor"
+echo "====================================================="
 sudo apt-get remove -y nano
 
-echo "==================================="
-echo "Replace vim.tiny with vim"
-echo "==================================="
+echo "====================================================="
+echo "replace vim.tiny with vim"
+echo "====================================================="
 sudo apt-get remove -y vim.tiny
 pip install neovim
 npm install -g neovim
 sudo apt-get install -y xclip
 sudo apt-get install -y ripgrep
 
-echo "==================================="
-echo "Installing untility softwares"
-echo "==================================="
-sudo snap install --classic alacritty
-sudo snap install dbeaver-ce
-sudo apt-get install -y tmux
-sudo apt-get install -y zsh
-sudo apt-get install -y htop
-sudo apt-get install -y http
-sudo apt-get install -y ranger
-sudo apt-get install -y git
-sudo apt-get install -y curl
-sudo apt-get install -y mpv
+echo "====================================================="
+echo "installing untility softwares"
+echo "====================================================="
+sudo snap    install --classic alacritty
+sudo snap    install           dbeaver-ce
+sudo apt-get install -y        tmux
+sudo apt-get install -y        zsh
+sudo apt-get install -y        htop
+sudo apt-get install -y        http
+sudo apt-get install -y        ranger
+sudo apt-get install -y        git
+sudo apt-get install -y        curl
+sudo apt-get install -y        mpv
 
-echo "==================================="
-echo "Installing misc softwares"
-echo "==================================="
+echo "====================================================="
+echo "installing misc softwares"
+echo "====================================================="
 sudo apt-get install -y neofetch
 
 echo "==================================="
@@ -44,20 +49,20 @@ echo "Installing Python development softwares"
 echo "==================================="
 sudo apt-get install -y python3-pip
 
-echo "==================================="
-echo "Install VSCodium (opensourced version of VSCode"
-echo "==================================="
+echo "====================================================="
+echo "install vscodium (opensourced version of vscode"
+echo "====================================================="
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
-sudo apt-get update -y
+sudo apt-get update  -y
 sudo apt-get install -y codium
 
-echo "==================================="
-echo "Installing common Desktop apps"
-echo "==================================="
+echo "====================================================="
+echo "installing common desktop apps"
+echo "====================================================="
 sudo apt-get install -y gnome-disk-utility
 sudo apt-get install -y gnome-tweak
 sudo apt-get install -y blender
@@ -65,18 +70,29 @@ sudo apt-get install -y krita
 sudo apt-get install -y gimp
 sudo apt-get install -y kdenlive
 
-echo "==================================="
-echo "Installing youtube downloader"
-echo "===================================";
+echo "====================================================="
+echo "installing youtube downloader"
+echo "====================================================="
 sudo add-apt-repository ppa:tomtomtom/yt-dlp
-sudo apt-get update -y
+sudo apt-get update  -y
 sudo apt-get install -y yt-dlp
 
-echo "==================================="
-echo "Installing brave browser"
-echo "==================================="
+echo "====================================================="
+echo "installing brave browser"
+echo "====================================================="
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt-get update -y
+sudo apt-get update  -y
 sudo apt-get install -y brave-browser
+
+echo "====================================================="
+echo "place config files in their appropriate locations"
+echo "====================================================="
+ln -s ./init.vim       /home/${whoami}/.config/nvim/init.vim
+ln -s ./alacritty.toml /home/${whoami}/.config/alacritty/alacritty.toml
+ln -s ./.tmux.conf     /home/${whoami}/.tmux.conf
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm /home/${whoami}/.zshrc;
+ln -s ./.zshrc /home/${whoami}/.zshrc
 
